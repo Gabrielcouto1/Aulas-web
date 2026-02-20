2053424b119d533c723e9ed2cf5a4a0dfaabff95
Gabriel Gabriel Freitas <couto@techers.local>
2026-02-20 17:42:47 -0300

    exercicio da calculadora de area

--- aula3/app.js
+++ aula3/app.js
@@ -1,1 +1,8 @@
 console.log("hello world!")
+
+// Calculadora de área dum terreno (largura * comprimento)
+function calculaArea(largura, comprimento){
+    return(largura * comprimento)
+}
+
+console.log(calculaArea(100, 40))
